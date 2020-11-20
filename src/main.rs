b5f9dccf73098df290ec05f0dc9118760f241754
Toshiaki Ishikura <craccho@gmail.com>
2020-11-21 07:15:09 +0900

    turns in Turn

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,43 @@ impl Turn {
             ep: [ep[3], ep[2], ep[1], ep[0]],
         }
     }
+
+    const R: Turn = Turn {
+        cp: [[ 7, 1], [ 4, 2], [ 3, 1], [ 0, 2]],
+        ep: [[ 4, 0], [ 8, 0], [ 7, 0], [ 0, 0]],
+    };
+    const RI: Turn = Turn::R.inverse();
+
+    const L: Turn = Turn {
+        cp: [[ 2, 2], [ 5, 1], [ 6, 2], [ 1, 1]],
+        ep: [[ 6, 0], [10, 0], [ 5, 0], [ 2, 0]],
+    };
+    const LI: Turn = Turn::L.inverse();
+
+    const U: Turn = Turn {
+        cp: [[ 3, 0], [ 0, 0], [ 1, 0], [ 2, 0]],
+        ep: [[ 3, 0], [ 2, 0], [ 1, 0], [ 0, 0]],
+    };
+    const UI: Turn = Turn::U.inverse();
+
+    const D: Turn = Turn {
+        cp: [[ 7, 0], [ 6, 0], [ 5, 0], [ 4, 0]],
+        ep: [[11, 0], [10, 0], [ 9, 0], [ 8, 0]],
+    };
+    const DI: Turn = Turn::D.inverse();
+
+    const F: Turn = Turn {
+        cp: [[ 1, 2], [ 6, 1], [ 7, 2], [ 0, 1]],
+        ep: [[ 5, 1], [11, 1], [ 4, 1], [ 1, 1]],
+    };
+    const FI: Turn = Turn::F.inverse();
+
+    const B: Turn  = Turn {
+        cp: [[ 3, 2], [ 4, 1], [ 5, 2], [ 2, 1]],
+        ep: [[ 6, 1], [ 3, 1], [ 7, 1], [ 9, 1]],
+    };
+    const BI: Turn = Turn::B.inverse();
+
 }
 
 impl Cube {
@@ -45,42 +82,6 @@ impl Cube {
             [ 0, 1,  2,  3,  4,  5, 6, 7, 8,  9, 10,11, 12, 13, 14,15, 16, 17,18,19, 20, 21,22,23],
             [19, 7, 11, 15, 18, 23, 8, 1, 6, 22, 12, 2, 10, 21, 16, 3, 14, 20, 4, 0, 17, 13, 9, 5],
         ];
-
-    const R: Turn = Turn {
-        cp: [[ 7, 1], [ 4, 2], [ 3, 1], [ 0, 2]],
-        ep: [[ 4, 0], [ 8, 0], [ 7, 0], [ 0, 0]],
-    };
-    const RI: Turn = Cube::R.inverse();
-
-    const L: Turn = Turn {
-        cp: [[ 2, 2], [ 5, 1], [ 6, 2], [ 1, 1]],
-        ep: [[ 6, 0], [10, 0], [ 5, 0], [ 2, 0]],
-    };
-    const LI: Turn = Cube::L.inverse();
-
-    const U: Turn = Turn {
-        cp: [[ 3, 0], [ 0, 0], [ 1, 0], [ 2, 0]],
-        ep: [[ 3, 0], [ 2, 0], [ 1, 0], [ 0, 0]],
-    };
-    const UI: Turn = Cube::U.inverse();
-
-    const D: Turn = Turn {
-        cp: [[ 7, 0], [ 6, 0], [ 5, 0], [ 4, 0]],
-        ep: [[11, 0], [10, 0], [ 9, 0], [ 8, 0]],
-    };
-    const DI: Turn = Cube::D.inverse();
-
-    const F: Turn = Turn {
-        cp: [[ 1, 2], [ 6, 1], [ 7, 2], [ 0, 1]],
-        ep: [[ 5, 1], [11, 1], [ 4, 1], [ 1, 1]],
-    };
-    const FI: Turn = Cube::F.inverse();
-
-    const B: Turn  = Turn {
-        cp: [[ 3, 2], [ 4, 1], [ 5, 2], [ 2, 1]],
-        ep: [[ 6, 1], [ 3, 1], [ 7, 1], [ 9, 1]],
-    };
-    const BI: Turn = Cube::B.inverse();
 
     fn turn(&mut self, t: &Turn) {
         let l = t.cp.len();
@@ -111,32 +112,32 @@ impl fmt::Display for Cube {
 fn main() {
 
     let mut cube: Cube = Cube::SOLVED;
-    let sequence = [Cube::R, Cube::U, Cube::RI, Cube::UI];
-    let sequence2 = [Cube::L, Cube::B, Cube::R, Cube::U, Cube::F, Cube::D];
-    let sequence3 = [Cube::DI, Cube::FI, Cube::UI, Cube::RI, Cube::BI, Cube::LI];
+    let sequence = [Turn::R, Turn::U, Turn::RI, Turn::UI];
+    let sequence2 = [Turn::L, Turn::B, Turn::R, Turn::U, Turn::F, Turn::D];
+    let sequence3 = [Turn::DI, Turn::FI, Turn::UI, Turn::RI, Turn::BI, Turn::LI];
     let sequence4 = [
-        Cube::R, Cube::RI,
-        Cube::L, Cube::LI,
-        Cube::U, Cube::UI,
-        Cube::D, Cube::DI,
-        Cube::F, Cube::FI,
-        Cube::B, Cube::BI,
+        Turn::R, Turn::RI,
+        Turn::L, Turn::LI,
+        Turn::U, Turn::UI,
+        Turn::D, Turn::DI,
+        Turn::F, Turn::FI,
+        Turn::B, Turn::BI,
     ];
     let sequence5 = [
-        Cube::R,
-        Cube::L,
-        Cube::U,
-        Cube::D,
-        Cube::F,
-        Cube::B,
-        Cube::BI,
-        Cube::FI,
-        Cube::DI,
-        Cube::UI,
-        Cube::LI,
-        Cube::RI,
+        Turn::R,
+        Turn::L,
+        Turn::U,
+        Turn::D,
+        Turn::F,
+        Turn::B,
+        Turn::BI,
+        Turn::FI,
+        Turn::DI,
+        Turn::UI,
+        Turn::LI,
+        Turn::RI,
     ];
-let sequence6 = [ Cube::R, Cube::U, Cube::U, Cube::DI, Cube::B, Cube::DI ];
+let sequence6 = [ Turn::R, Turn::U, Turn::U, Turn::DI, Turn::B, Turn::DI ];
     let mut ct = 0;
 
     println!("initial state: {}", cube.to_string());
